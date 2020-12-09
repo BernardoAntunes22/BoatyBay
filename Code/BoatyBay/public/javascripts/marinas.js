85df82f2f8ee29b1cacdd26b66e4b4fd680df09d
BAntunes <Bernardo.salgueiro.antunes@gmail.com>
2020-12-09 16:46:43 +0000

    Inital Code

---
+++ Code/BoatyBay/public/javascripts/marinas.js
@@ -0,0 +1,36 @@
+const marinas= [
+    {
+        name: 'Marina de Vilaoura',
+        capacity: '1000'
+    },
+
+    {
+        name: 'Doca de Santo',
+        capacity: 200
+    }
+];
+
+
+function createCards() {
+    let main = document.getElementById('marinas');
+    for (let idx in marinas) {
+        main.innerHTML += makeCard(idx);
+    }
+}
+
+function makeCard(idxmarinas) {
+    return `<section class="marinasL">
+        <h2>
+            ${marinas[idxmarinas].name}
+        </h2>
+        <p>
+            Number: ${marinas[idxmarinas].capacity}
+        </p>
+        
+    </section>`;
+}
+
+window.onload = () => {
+    createCards();
+
+}
